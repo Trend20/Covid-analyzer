165de5bed2637b22811ca197219ffb42f14f6de0
Trend20 <enockomondi305@gmail.com>
2020-04-10 07:53:40 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+
+import styles from './App.module.css';
+
+import {fetchData } from './api';
+
+class App extends React.Component{
+    state={
+        data:{},
+        country: ''
+    }
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) =>{
+        console.log(country);
+        // fetch data
+        // set the state
+    }
+    render(){
+
+        const { data } = this.state;
+
+        return(
+            <div className={styles.container}>
+             <Cards data={ data } />
+             <CountryPicker />
+             <Chart />
+            </div>
+        )
+    }
+}
+
+export default App;
